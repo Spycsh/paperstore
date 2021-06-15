6a7150e8586ef6f58ee3fb2bbf4f830e7b748cbd
Spycsh <757407490@qq.com>
2021-06-15 11:00:12 +0200

    sql

--- priv/queries.sql
+++ priv/queries.sql
@@ -1,13 +1,26 @@
+-- :setup_paper_store
 CREATE TABLE papers (
     doi varchar(40) PRIMARY KEY
     title varchar(256) NOT NULL,
     author varchar(256) NOT NULL,
+    lock boolean,
+    price int,
+    link varchar(256)
 );
 
+-- :teardown_paper_store
 DROP TABLE papers;
 
 -- add a paper
+-- :add_paper
 INSERT INTO papers  (doi, title, author)
     VALUES          ($1,  $2,   $3);
 
+-- :find_by_author
+SELECT * FROM papers WHERE author LIKE $1;
 
+-- :find_by_title
+SELECT * FROM papers WHERE title LIKE $1;
+
+-- :find_by_doi
+SELECT * FROM papers WHERE doi = $1;
